72b8adf6002eb50ebcb59fe8473855aace8e140d
Billan Abdi <billanabdi04@gmail.com>
2023-11-05 18:54:12 +0400

    Solved again

--- challenge.js
+++ challenge.js
@@ -1,11 +1,18 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
 
-var tip;
-tip = 150;
+let bill = 160;
+let tip;
 
-if (tip >= 50 && tip <= 300) {
-  console.log(tip % 20);
+if (bill <= 50 && bill >= 300) {
+  tip = bill * 0.15;
+  console.log(
+    `The bill is ${bill} and the tip is ${tip} the cost is ${bill + tip}`
+  );
 } else {
-  console.log(tip % 15);
+  bill > 300;
+  tip = bill * 0.2;
+  console.log(
+    `The bill is ${bill} and the tip is ${tip} the cost is ${bill + tip}`
+  );
 }
